7107b8ee77b92081b9e0dedc4ed525936228f08f
chlamydamanada <evil.cabaret@gmail.com>
2022-11-28 14:53:37 +0100

    test7

--- src/repositories/all_data_db_repository.ts
+++ src/repositories/all_data_db_repository.ts
@@ -3,10 +3,10 @@ import { blogsCollection, postsCollection } from "./db";
 export const allDataRepository = {
   async deleteAllPost() {
     let isDel = await postsCollection.deleteMany({});
-    return isDel.deletedCount > 0;
+    return isDel.deletedCount >= 0;
   },
   async deleteAllBlogs() {
     let isDel = await blogsCollection.deleteMany({});
-    return isDel.deletedCount > 0;
+    return isDel.deletedCount >= 0;
   },
 };
